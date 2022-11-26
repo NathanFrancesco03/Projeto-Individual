a8c0f6ba9cc0d03153a9c39460341c2aae153d97
NathanFrancesco03 <nathan.sepulvida@sptech.school>
2022-11-25 21:43:41 -0300

    Arrumando validações

--- projeto_santos/site/src/models/avisoModel.js
+++ projeto_santos/site/src/models/avisoModel.js
@@ -12,7 +12,7 @@ function listar() {
 function cronometro() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        select fkusuario, nome, cronometrar from jogomemoria order by cronometrar desc limit 5;
+        select fkusuario, nome, cronometrar from jogomemoria order by cronometrar asc limit 5;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
